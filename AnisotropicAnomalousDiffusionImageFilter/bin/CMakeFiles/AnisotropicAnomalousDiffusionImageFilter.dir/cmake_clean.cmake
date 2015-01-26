fdc7733163969f329c581a2f48a0b2b47da72066
Antonio Carlos da Silva Senra Filho <acsenrafilho@gmail.com>
2015-01-26 18:25:00 -0200

    IAD and AAD folders added.

---
+++ AnisotropicAnomalousDiffusionImageFilter/bin/CMakeFiles/AnisotropicAnomalousDiffusionImageFilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AnisotropicAnomalousDiffusionImageFilter.dir/Main.o"
+  "AnisotropicAnomalousDiffusionImageFilter.pdb"
+  "AnisotropicAnomalousDiffusionImageFilter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnisotropicAnomalousDiffusionImageFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ IsotropicAnomalousDiffusionImageFilter/bin/CMakeFiles/IsotropicAnomalousDiffusionImageFilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IsotropicAnomalousDiffusionImageFilter.dir/Main.o"
+  "IsotropicAnomalousDiffusionImageFilter.pdb"
+  "IsotropicAnomalousDiffusionImageFilter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IsotropicAnomalousDiffusionImageFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
